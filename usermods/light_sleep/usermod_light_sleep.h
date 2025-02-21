098c5eadb5411c73165e58aa40aa44122ab7bd08
Damian Schneider <daedae@gmx.ch>
2025-02-21 18:22:03 +0100

    many more experiments, getting closer to a working version

--- usermods/light_sleep/usermod_light_sleep.h
+++ usermods/light_sleep/usermod_light_sleep.h
@@ -13,6 +13,7 @@ class LightSleepUsermod : public Usermod {
 
   private:
     bool enabled = false;
+    bool skipSleep = false; // skips light sleep (set in UM config) in case of issues, still uses wifi-sleep and lower CPU clock in off mode
     bool didSleep = false;
     bool wakeUp = false;
     
@@ -31,14 +32,22 @@ class LightSleepUsermod : public Usermod {
         return;
 
       if(!offMode) {
+        if(skipSleep) {
+          pinMode(8, OUTPUT);  // DEBUG output -> led on GPIO8 (C3 supermini)
+         digitalWrite(8, HIGH);  // TODO: why is power consumption lower when using a delay here?
+      delay(50);  // a delay is needed to properly handle wifi stuff, connection is lost if this is set lower than 50 ms
+      digitalWrite(8, LOW);
+      delay(50);
+        }
+
         if(didSleep) { // after wake-up, set wifi-sleep back to user setting
           //WiFi.setSleep(!noWifiSleep);
           //WLED::instance().initConnection(); // re-init connection (sets wifi sleep mode to user value, if set manually here, it can sometimes cause wifi issues)
           //TODO: initConnection is quite slow, after wake-up, UI is unresponsive for some time... maybe do a quick re-connect right here?
           //WiFi.disconnect();           // disconnect from wifi
-          //delay(5);
-          WiFi.setSleep(!noWifiSleep); // set user setting
-          delay(10);
+          delay(5);
+          WiFi.setSleep(!noWifiSleep); // set user set ting
+          delay(5);
           #if defined(CONFIG_IDF_TARGET_ESP32C3) // ESP32 C3
             setCpuFrequencyMhz(160);      // set CPU frequency back to 160MHz
           #else
@@ -53,19 +62,30 @@ class LightSleepUsermod : public Usermod {
       }
       pinMode(8, OUTPUT);  // DEBUG output -> led on GPIO8 (C3 supermini)
       digitalWrite(8, HIGH);  // TODO: why is power consumption lower when using a delay here?
-      delay(10);  
+      delay(50);  // a delay is needed to properly handle wifi stuff, connection is lost if this is set lower than 50 ms
       digitalWrite(8, LOW);
-      // if we are in off mode, enable wifi sleep and enter light sleep
-      if(millis() > LIGHTSLEEP_STARTUPDELAY) {
+      digitalWrite(7, HIGH); // debug, relay on
+
+      static int skipcounter = 0;
+
+      // if we are in off mode, enable wifi sleep and spend some time in light sleep
+      if(millis() > LIGHTSLEEP_STARTUPDELAY) { // wait a bit before going to sleep
+        if(skipcounter > 0) {
+          skipcounter--;
+          return;
+        }
+        wakeUp = false;
         if(!didSleep) { // first time sleep call
-          WiFi.setSleep(true); // save power by enabling wifi auto sleep
-          setCpuFrequencyMhz(80);
-          //wakeUp = false;
+          setCpuFrequencyMhz(80); // slow down CPU to 80MHz to save power (80MHz is lowest possible frequency with wifi enabled)
         }
 
+        if(WiFi.getSleep() != WIFI_PS_MIN_MODEM) { // check if wifi sleep is enabled, enable it if it is not
+          WiFi.setSleep(WIFI_PS_MIN_MODEM); // save power by enabling wifi auto sleep
+          delay(50); // make sure wifi is stable before going to sleep (not sure this is needed)
+        }
 
          //TODO: how to handle local time? needs a timer to keep track of time and update millis() after wake-up?
-        //while(!wakeUp) {
+       // while(!wakeUp && offMode) {
           // enable wakeup on any configured button pins
           for(int i = 0; i < WLED_MAX_BUTTONS; i++) {
             if(btnPin[i] >= 0 && buttonType[i] > BTN_TYPE_RESERVED && buttonType[i] < BTN_TYPE_TOUCH) { // TODO: add touch button support for S3 and S2
@@ -86,28 +106,46 @@ class LightSleepUsermod : public Usermod {
               #endif
             }
           }
-          esp_sleep_enable_timer_wakeup(1000000);  // fallback: wake up every second
-          esp_sleep_enable_wifi_wakeup(); // note: not avialable on classic ESP32
-          esp_light_sleep_start();  // Enter light sleep
-
+          esp_sleep_enable_timer_wakeup(2000000);  // fallback: wake up every 2 seconds to check wifi
+          esp_sleep_enable_wifi_wakeup(); // note: not avialable on classic ESP32 -> wakes up every beacon inverval
+          if (!apActive && !skipSleep) // do not go to sleep in AP mode, it will disconnect
+            esp_light_sleep_start();  // Enter light sleep  No light sleep: about 30mA, with light sleep: 15mA
+          else
+            skipcounter = 5000;
           // we woke up, check if we should go back to sleep
           //check wakeup reason
           /*
           esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
-          if(wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
-
+          if(wakeup_reason == ESP_SLEEP_WAKEUP_WIFI) {
+           delay(5); // process wifi stuff before going back to sleep
+           wakeUp = true; //!offMode; // wake up if wifi changed the state
           }
           else
-            wakeUp = true; // wake up if button or wifi triggered
-*/
-      //  }
+          {
+              wakeUp = true; // wake up if button or timeout triggered and run the main loop
+          }*/
+
+          if(WLED_WIFI_CONFIGURED) {
+            if(WiFi.status() != WL_CONNECTED) { //not connected, reconnect
+              //WiFi.reconnect();
+              //WLED::instance().handleConnection(); // check wifi state, re-connect if necessary
+              WLED::instance().initConnection();
+              skipcounter = 2000; // run main loop a few times before going back to sleep (otherwise wifi breaks) 200 works for wifi but AP does not start, 2000 works with AP
+              }
+          }
+
+        //}
         didSleep = true;
-        // note: when skipping the below lines, connection can be lost after some time so better keep this
-        if(!WiFi.isConnected()) { // TODO: this only checks STA mode, need to handle AP mode as well 
-          WLED::instance().initConnection(); // re-init connection (sets wifi sleep mode to user value)  TODO: is there a better, faster way to check and re-connect?
-          didSleep = false; // reset sleep flag to re-enable wifi sleep if set by user
-        }
-        WLED::instance().handleConnection(); // check wifi state, re-connect if necessary
+        // note: when skipping the below lines, connection can be lost after some time so better keep this -> TODO: is there a better way? if wifi is down, it reboots when having this code
+        // BUT: when not having it, connection is lost, ESPNow still works though so something is wrong with the wifi connection
+
+
+       // if(!WiFi.isConnected()) { // TODO: this only checks STA mode, need to handle AP mode as well 
+       // WLED::instance().initConnection(); // re-init connection (sets wifi sleep mode to user value)  TODO: is there a better, faster way to check and re-connect?
+        //  didSleep = false; // reset sleep flag to re-enable wifi sleep if set by user
+       // }
+        //forceReconnect = true; //TODO: does this help with lost connection?
+        //WLED::instance().handleConnection(); // check wifi state, re-connect if necessary
         
       }
     }
@@ -124,6 +162,7 @@ class LightSleepUsermod : public Usermod {
     {
       JsonObject top = root.createNestedObject(FPSTR(_name));
       top[FPSTR(_enabled)] = enabled;
+      top["skipSleep"] = skipSleep;
     }
 
     bool readFromConfig(JsonObject& root) override
@@ -131,7 +170,18 @@ class LightSleepUsermod : public Usermod {
       JsonObject top = root[FPSTR(_name)];
       bool configComplete = !top.isNull();
       configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled);
+      configComplete &= getJsonValue(top["skipSleep"], skipSleep, false);
       return configComplete;
+    }
+
+    /*
+     * appendConfigData() is called when user enters usermod settings page
+     * it may add additional metadata for certain entry fields (adding drop down is possible)
+     * be careful not to add too much as oappend() buffer is limited to 3k
+     */
+    void appendConfigData() override
+    {
+      oappend(SET_F("addInfo('LightSleep:skipSleep',1,'(no LightSleep, only WiFiSleep)','');")); // first string is suffix, second string is prefix
     }
 
     /*
