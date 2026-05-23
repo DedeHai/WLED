80e21d3dfb26b106a9c1da4a3613b03d91a0ec70
Damian Schneider <daedae@gmx.ch>
2026-05-23 08:10:30 +0200

    fix number of digital channels in UI, better support for ESP32

--- wled00/const.h
+++ wled00/const.h
@@ -99,8 +99,8 @@ constexpr size_t FIXED_PALETTE_COUNT = DYNAMIC_PALETTE_COUNT + FASTLED_PALETTE_C
     //#define WLED_MAX_ANALOG_CHANNELS 16
     #define WLED_PLATFORM_ID 4       // used in UI to distinguish ESP type in UI, needs a proper fix!
   #endif
-  #define WLED_MAX_DIGITAL_CHANNELS (WLED_MAX_RMT_CHANNELS + WLED_MAX_I2S_CHANNELS)
   #define WLED_MAX_BB_CHANNELS 8           // max parallel BitBang channels (GPIO 0-31 only)
+  #define WLED_MAX_DIGITAL_CHANNELS (WLED_MAX_RMT_CHANNELS + WLED_MAX_I2S_CHANNELS + WLED_MAX_BB_CHANNELS) // total number of digital channels (RMT + I2S + BitBang)
 #endif
 // WLED_MAX_BUSSES was used to define the size of busses[] array which is no longer needed
 // instead it will help determine max number of buses that can be defined at compile time
